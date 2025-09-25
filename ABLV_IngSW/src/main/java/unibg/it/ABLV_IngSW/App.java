7605fd1b4c616240a51d9aa1395de8d2cad984df
Bebe102030 <168192331+MBertoli14@users.noreply.github.com>
2025-09-25 17:05:29 +0200

    pulizia

--- ABLV_IngSW/src/main/java/unibg/it/ABLV_IngSW/App.java
+++ ABLV_IngSW/src/main/java/unibg/it/ABLV_IngSW/App.java
@@ -6,20 +6,5 @@ package unibg.it.ABLV_IngSW;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
-        System.out.println("Hello World!");
-
-        System.out.println("Hello World!");
-
-        System.out.println("Hello World!");
-
-        System.out.println("Hello World!");
-
-
-        System.out.println("ciao da bebe");
-        System.out.println("ciao da algeri");
-        System.out.println("ciao sono cele");
-        System.out.println("forza atalanta, vai juric!");
-        System.out.println("complimenti al brembate per la promozione");
     }
 }
