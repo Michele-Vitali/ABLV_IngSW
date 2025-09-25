1302e7064b96bcceb68ce169416570ef02ba0101
Bebe102030 <168192331+MBertoli14@users.noreply.github.com>
2025-09-25 16:49:06 +0200

    Update App.java

--- ABLV_IngSW/src/main/java/unibg/it/ABLV_IngSW/App.java
+++ ABLV_IngSW/src/main/java/unibg/it/ABLV_IngSW/App.java
@@ -7,7 +7,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-
+        System.out.println("ciao da bebe");
         System.out.println("ciao da algeri");
         System.out.println("ciao sono cele");
     }
