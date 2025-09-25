751c5d6c9913e2c30dbb1da3af9498700a0b1846
Algeri4 <n.algeri1@studenti.unibg.it>
2025-09-25 16:35:36 +0200

    ciao da algeri
    descrizione

--- ABLV_IngSW/src/main/java/unibg/it/ABLV_IngSW/App.java
+++ ABLV_IngSW/src/main/java/unibg/it/ABLV_IngSW/App.java
@@ -6,5 +6,6 @@ package unibg.it.ABLV_IngSW;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("ciao da algeri");
     }
 }
