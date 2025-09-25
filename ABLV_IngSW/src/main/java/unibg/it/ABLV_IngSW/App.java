f9c7a3810bf5ce33ff03c1fe0ec17618f4ef8799
Michele <79157568+Michele-Vitali@users.noreply.github.com>
2025-09-25 16:55:31 +0200

    Update App.java

--- ABLV_IngSW/src/main/java/unibg/it/ABLV_IngSW/App.java
+++ ABLV_IngSW/src/main/java/unibg/it/ABLV_IngSW/App.java
@@ -6,7 +6,7 @@ package unibg.it.ABLV_IngSW;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
+        int ciao = 0;
         System.out.println("ciao da bebe");
         System.out.println("ciao da algeri");
         System.out.println("ciao sono cele");
